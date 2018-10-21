ed0795c56b1b0bfb325fc2d04ee35df16a1cbcbb
gudi99 <vnaykumr0@gmail.com>
2018-10-21 19:10:42 +0530

    don't import so many only one import FASAK

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import "semantic-ui-css/semantic.min.css";
-import App from "./App";
-import rootReducer from "./rootReducer";
+irootReducer";
 import registerServiceWorker from "./registerServiceWorker";
 
 const store = createStore(
